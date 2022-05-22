a7e18be455e15265512f36e637b5c347d1ab70cb
yuziwei <yuziwei@blacklake.cn>
2022-05-22 19:18:57 +0800

    feat(all):init

--- packages/main-react/src/index.js
+++ packages/main-react/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { registerMicroApps, start,loadMicroApp } from 'qiankun';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -15,3 +16,29 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+/**
+ * 微应用注册
+ */
+registerMicroApps([
+  {
+    name: 'sub-react', // app name registered
+    entry: process.env.REACT_APP_aaa,
+    container: '#root',
+    activeRule: '/my-sub-react',
+  },
+]);
+
+start();
+/**
+ * 微应用注册 END
+ */
+
+/**
+ * 与路由不相关，手动注册
+ */
+//  loadMicroApp({
+//   name: 'app',
+//   entry: '//localhost:3002',
+//   container: '#subRoot',
+// });
