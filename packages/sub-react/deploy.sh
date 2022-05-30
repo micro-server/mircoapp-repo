1522fac1ca0823fff158fcec31b3c758d631fab7
yuziwei <yuziwei@blacklake.cn>
2022-05-30 10:24:37 +0800

    feat(deploy):更新部署配置文件

--- packages/sub-react/deploy.sh
+++ packages/sub-react/deploy.sh
@@ -6,6 +6,6 @@ npm run build
 
 build_time=`date '+%Y%m%d%H%M%S'`
 
-mv /etc/nginx/build/sub /etc/nginxbuildHistory/build-$build_time
+mv /etc/nginx/sub /etc/nginxbuildHistory/build-$build_time
 
 mv build /etc/nginx/sub
